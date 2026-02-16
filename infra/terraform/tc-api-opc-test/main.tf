a5b3628f6f64e46148b789c724f875b514200815
sadatmalik <sadat.malik@me.com>
2026-02-16 14:24:33 +0000

    update tc_api_url for opc test environment

--- infra/terraform/tc-api-opc-test/main.tf
+++ infra/terraform/tc-api-opc-test/main.tf
@@ -32,7 +32,7 @@ module tc-opc-test {
   site_domain         = "test.api.plus.tctalent.org"
 
   # SSM-backed (stored and managed in SSM, injected into ECS) – set via ssm-parameters.sh
-  tc_api_url              = "https://tctalent-test.org/api/admin"
+  tc_api_url              = "https://test.plus.tctalent.org/api/admin"
   tc_search_id        = 2682
   tc_username         = "tc-api"
   tc_password         = "" # set via ssm-parameters.sh
