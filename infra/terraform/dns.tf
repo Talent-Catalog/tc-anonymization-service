25139789cd7ea3576f1f281875a55cff21122ca6
sadatmalik <sadat.malik@me.com>
2026-02-19 15:41:17 +0000

    add Route 53 records for domain and www CNAME

--- infra/terraform/dns.tf
+++ infra/terraform/dns.tf
@@ -8,6 +8,27 @@ resource "aws_route53_zone" "this" {
   tags = local.tags
 }
 
+# A record pointing the domain to the ALB
+resource "aws_route53_record" "ipv4" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = var.site_domain
+  type    = "A"
+
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "www"
+  type    = "CNAME"
+  ttl     = "600"
+  records = [var.site_domain]
+}
+
 # DNS validation records for ACM certificate
 resource "aws_route53_record" "certificate_validation" {
   for_each = {
