ae93e88a13e57d7468bef6a4bc02f6b4d9b4a9df
sadatmalik <sadat.malik@me.com>
2026-02-19 15:42:20 +0000

    move DNS validation record for ACM certificate to acm.tf

--- infra/terraform/dns.tf
+++ infra/terraform/dns.tf
@@ -28,20 +28,3 @@ resource "aws_route53_record" "www" {
   ttl     = "600"
   records = [var.site_domain]
 }
-
-# DNS validation records for ACM certificate
-resource "aws_route53_record" "certificate_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = aws_route53_zone.this.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 60
-  records = [each.value.record]
-}
